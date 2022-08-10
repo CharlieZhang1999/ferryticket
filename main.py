b58d400993f80d3da3f7f0b855efdc47d6468001
CharlieZhang1999 <qiuyang@live.unc.edu>
2022-08-10 08:56:45 +0800

    don't bark

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ import time
 # Github Action
 enable_gh_action = False  # 启用GitHub Action
 # Bark Push
-enable_bark = True  # 启用Bark推送
-bark_token = "ZEuy8diSuFYBGPFgs5iBL4/有票了?sound=alarm"  # Bark推送ID
+enable_bark = False  # 启用Bark推送
+bark_token = ""  # Bark推送ID
 # Serverchan Push
 enable_serverchan = False  # 启用Server酱(Turbo)推送
 sct_token = ""  # Sendkey
@@ -25,7 +25,7 @@ sct_token = ""  # Sendkey
 startSite = "SK"  # 始发站点
 endSite = "HKA"  # 目标站点
 startDate = "2022-08-24"  # 船票搜索日期
-endDate = "2022-08-30"
+endDate = "2022-08-24"
 show_available_only = True  # 只显示有票的日期
 iteration = True
 # ==================================
